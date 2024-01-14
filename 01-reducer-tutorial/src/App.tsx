f49de0eae40518515c61a578bb36aaa883c995ca
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-01-14 13:48:06 +0100

    base structure, router

--- 01-reducer-tutorial/src/App.tsx
+++ 01-reducer-tutorial/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Checkout } from './views/Checkout/Checkout';
 import './App.css';
 
 function App() {
-	return <div>app</div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Checkout />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
