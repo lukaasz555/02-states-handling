b1de0ad10733e3a7c76b7fa002c9392b39604180
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-01-15 21:01:35 +0100

    init 02

---
+++ 02-auth-context/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './views/Home';
+import { Login } from './views/Login';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
