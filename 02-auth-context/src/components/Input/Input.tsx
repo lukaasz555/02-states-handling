6a7e2f3e9e81b66819b1e3984e839bab5b2c5f38
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-01-16 10:55:18 +0100

    loginform ui

---
+++ 02-auth-context/src/components/Input/Input.tsx
@@ -0,0 +1,37 @@
+import './Input.css';
+
+interface InputProps {
+	value: string;
+	name: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	type?: string;
+	isDisabled?: boolean;
+	placeholder?: string;
+}
+
+export const Input = ({
+	value,
+	onChange,
+	name,
+	type = 'text',
+	isDisabled = false,
+	placeholder,
+}: InputProps) => {
+	console.log(value, onChange, type, isDisabled, placeholder);
+
+	return (
+		<div className='input__wrapper'>
+			<label>
+				<p>{name.charAt(0).toUpperCase() + name.substring(1)}:</p>
+				<input
+					value={value}
+					name={name}
+					type={type}
+					disabled={isDisabled}
+					placeholder={placeholder}
+					onChange={onChange}
+				/>
+			</label>
+		</div>
+	);
+};
--- 02-auth-context/src/components/LoginForm/LoginForm.tsx
+++ 02-auth-context/src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+import { Input } from '../Input/Input';
 import './LoginForm.css';
 
 export const LoginForm = () => {
-	return <div>here goes the form...</div>;
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const handleFormUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+	return (
+		<div className='form__wrapper'>
+			<Input value={formData.email} name='email' onChange={handleFormUpdate} />
+			<Input
+				value={formData.password}
+				name='password'
+				onChange={handleFormUpdate}
+			/>
+		</div>
+	);
 };
