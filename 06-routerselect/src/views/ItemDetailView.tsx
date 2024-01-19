80163e3bae6851e0fb0dff96f6b837b8513e53bc
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-01-19 21:59:11 +0100

    ui / mockdata

--- 06-routerselect/src/views/ItemDetailView.tsx
+++ 06-routerselect/src/views/ItemDetailView.tsx
@@ -1,7 +1,17 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export const ItemDetailView = () => {
+	const navigate = useNavigate();
 	const { id } = useParams();
 
-	return <div>ItemDetailView {id}</div>;
+	// get itemById from itemsStore or sth
+
+	return (
+		<div>
+			<header style={{ marginBottom: '3em' }}>
+				<button onClick={() => navigate(-1)}>go back</button>
+			</header>
+			<main>ItemDetailView {id}</main>
+		</div>
+	);
 };
--- 06-routerselect/src/views/ItemsListView.tsx
+++ 06-routerselect/src/views/ItemsListView.tsx
@@ -1,1 +1,29 @@
-export const ItemsListView = () => <div>ItemsListView</div>;
+import { useEffect, useState } from 'react';
+import { ItemCard } from '../components/ItemCard';
+import data from '../data/data.json';
+import { IProduct } from '../interfaces/IProduct';
+
+export const ItemsListView = () => {
+	const [items, setItems] = useState<IProduct[]>([]);
+
+	useEffect(() => {
+		setItems(data.items);
+	}, []);
+
+	//! TODO: receive items from itemsStore or sth instead of mock json
+
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				rowGap: '1em',
+				maxWidth: '600px',
+				margin: '0 auto',
+			}}>
+			{items.map(({ id, name, price }) => (
+				<ItemCard key={id} id={id} name={name} price={price} />
+			))}
+		</div>
+	);
+};
